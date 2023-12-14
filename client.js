281cd62db70cb7b0e83da621b44a8feade064929
Viraj Desai <toviraj08@gmail.com>
2023-12-14 14:45:46 +0530

    Final

---
+++ client.js
@@ -0,0 +1,107 @@
+const socket = io("http://localhost:8000/", { transports: ['websocket', 'polling', 'flashsocket'] });
+
+var p = document.getElementById("connection_status");
+
+var btnSend = document.getElementById("btnSend");
+
+var btnStartStop = document.getElementById("btnStartStop");
+
+const waiting_msg = "Waiting for a Stranger to Connect.......";
+const disconnected_msg = "Stranger has been Vanished";
+const connected_msg = "Stranger Connected";
+const me = "You";
+const stranger = "Stranger";
+
+var roomId = null;
+
+socket.on("connect", () => {
+
+    socket.on("waiting-for-connection", conn => {
+        console.log("waiting for a Stranger to connect.......");
+        p.innerText = waiting_msg;
+    });
+    
+    socket.on("connection-successful", (id) => {
+        console.log("connection done: ", id);
+        p.innerText = connected_msg;
+        roomId = id;
+        btnSend.disabled = false;
+    });
+    
+    socket.on("message-received", (message) => {
+        console.log(message)
+        var txtChats = document.getElementById("txtChats");
+        var box = document.createElement("div");
+        box.className = "receivedChats";
+    
+        var msg = document.createElement("div");
+        msg.className = "msg";
+    
+        msg.appendChild(document.createTextNode(message));
+    
+        box.appendChild(msg);
+        
+        var identity = document.createElement("div");
+        identity.className = "identity";
+        identity.appendChild(document.createTextNode(stranger));
+        box.appendChild(identity);
+        
+        txtChats.appendChild(box);
+        
+        txtChats.scrollTop = txtChats.scrollHeight;
+    });
+    
+    socket.on("stranger-disconnected", () => {
+        roomId = null;
+        p.innerText = disconnected_msg;
+        btnStartStop.value = "Start";
+        btnSend.disabled = true;
+    });
+});
+
+
+function sendMsg() {
+    var txtMsg = document.getElementById("txtMsg").value;
+    var validated_msg = String(txtMsg);
+    if(validated_msg != ""){
+        socket.emit("message-sent", validated_msg, roomId);
+
+        var txtChats = document.getElementById("txtChats");
+        var box = document.createElement("div");
+        box.className = "sentChats";
+        
+        
+        var msg = document.createElement("div");
+        msg.className = "msg";
+        
+        msg.appendChild(document.createTextNode(validated_msg));
+        
+        box.appendChild(msg);
+        txtChats.appendChild(box);
+
+        var identity = document.createElement("div");
+        identity.className = "identity";
+        identity.appendChild(document.createTextNode(me));
+        box.appendChild(identity);
+
+        txtChats.scrollTop = txtChats.scrollHeight;
+    }
+}
+
+
+function startStopConnection(){
+
+    if(btnStartStop.value == "Start"){
+        socket.emit("start-connection");
+        btnStartStop.value = "Stop";
+    }
+    else {
+        btnStartStop.value = "Start";
+        p.innerText = "Stopped";
+        socket.emit("stranger-disconnected");
+    }
+}
+
+
+
+
---
+++ node_files/server.js
@@ -0,0 +1,99 @@
+const io = require("socket.io")(8000);
+
+const userId = [];
+const userConn = [];
+
+function remove(element){
+    let index = userId.indexOf(element);
+    userId.splice(index, 1);
+}
+
+io.on("connection", async (socket) => {
+    console.clear();
+
+    socket.on("start-connection", () => {
+        
+        if (!userId.includes(socket.id)) {
+            
+            userId.push(socket.id);
+        }
+    
+        if (userId.length == 2) {
+
+            socket.to(userId[0]).emit("connection-successful", socket.id);
+            socket.emit("connection-successful", userId[0]);
+            let socketId = socket.id;
+            let socketId2 = userId[0];
+            userConn.push({
+                socketId: socketId2,
+                socketId2: socketId
+            });
+
+            while(userId.length > 0){
+                userId.pop();
+            }
+        }
+        else {
+            socket.emit("waiting-for-connection");
+            console.log("waiting for a Stranger to connect.......");
+        }
+
+        console.log(userId, userConn);
+    });
+
+    socket.on("message-sent", (message, roomId) => {
+        message = String(message);
+        io.to(roomId).emit("message-received", message);
+        console.log("message: ", message);
+    });
+
+    socket.on("disconnect", () => {
+        let id = null;
+        remove(socket.id)
+        userConn.forEach(conn => {
+            if (conn["socketId"] == socket.id) {
+                id = conn["socketId2"];
+                console.log(id);
+                let index = userConn.indexOf(conn);
+                userConn.splice(index, 1);
+                return;
+            }   
+            else if (conn["socketId2"] == socket.id) {
+                id = conn["socketId"];
+                console.log(id);
+                let index = userConn.indexOf(conn);
+                userConn.splice(index, 1);
+                return;
+            }
+        });
+
+        io.to(id).emit("stranger-disconnected");
+        console.log("gaya: ", socket.id)
+    });
+
+    socket.on("stranger-disconnected", () => {
+        let id = null;
+        remove(socket.id);
+        userConn.forEach(conn => {
+            if (conn["socketId"] == socket.id) {
+                id = conn["socketId2"];
+                console.log(id);
+                let index = userConn.indexOf(conn);
+                userConn.splice(index, 1);
+                return;
+            }   
+            else if (conn["socketId2"] == socket.id) {
+                id = conn["socketId"];
+                console.log(id);
+                let index = userConn.indexOf(conn);
+                userConn.splice(index, 1);
+                return;
+            }
+        });
+        io.to(id).emit("stranger-disconnected");
+        
+        console.log(userId, userConn);
+        console.log("gaya: ", socket.id)
+    });
+
+});
