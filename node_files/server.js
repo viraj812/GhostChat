5c46d3ab243e4e12175f3fdd3d1ba83e5e4ed107
Viraj Desai <toviraj08@gmail.com>
2023-12-15 12:07:36 +0530

    update

--- node_files/server.js
+++ node_files/server.js
@@ -1,23 +1,27 @@
-const io = require("socket.io")(8882);
+const io = require("socket.io")(8382);
 
 const userId = [];
 const userConn = [];
 
-// function remove(element){
-//     let index = userId.indexOf(element);
-//     userId.splice(index, 1);
-// }
+function remove(e) {
+    let i = userId.indexOf(e);
+
+    if (i > -1) {
+        userId.splice(i, 1);
+    }
+}
+
 
 io.on("connection", async (socket) => {
     console.clear();
 
     socket.on("start-connection", () => {
-        
+
         if (!userId.includes(socket.id)) {
-            
+
             userId.push(socket.id);
         }
-    
+
         if (userId.length == 2) {
 
             socket.to(userId[0]).emit("connection-successful", socket.id);
@@ -29,7 +33,7 @@ io.on("connection", async (socket) => {
                 socketId2: socketId
             });
 
-            while(userId.length > 0){
+            while (userId.length > 0) {
                 userId.pop();
             }
         }
@@ -49,19 +53,20 @@ io.on("connection", async (socket) => {
 
     socket.on("disconnect", () => {
         let id = null;
-        
+        remove(socket.id);
+        console.log(userId);
         userConn.forEach(conn => {
             if (conn["socketId"] == socket.id) {
                 id = conn["socketId2"];
-                
+                remove(id);
                 console.log(id);
                 let index = userConn.indexOf(conn);
                 userConn.splice(index, 1);
                 return;
-            }   
+            }
             else if (conn["socketId2"] == socket.id) {
                 id = conn["socketId"];
-
+                remove(id);
                 console.log(id);
                 let index = userConn.indexOf(conn);
                 userConn.splice(index, 1);
@@ -75,19 +80,19 @@ io.on("connection", async (socket) => {
 
     socket.on("stranger-disconnected", () => {
         let id = null;
-    
+        remove(socket.id);
         userConn.forEach(conn => {
             if (conn["socketId"] == socket.id) {
                 id = conn["socketId2"];
-            
+                remove(id);
                 console.log(id);
                 let index = userConn.indexOf(conn);
                 userConn.splice(index, 1);
                 return;
-            }   
+            }
             else if (conn["socketId2"] == socket.id) {
                 id = conn["socketId"];
-            
+                remove(id);
                 console.log(id);
                 let index = userConn.indexOf(conn);
                 userConn.splice(index, 1);
@@ -95,7 +100,7 @@ io.on("connection", async (socket) => {
             }
         });
         io.to(id).emit("stranger-disconnected");
-        
+
         console.log(userId, userConn);
         console.log("gaya: ", socket.id)
     });
